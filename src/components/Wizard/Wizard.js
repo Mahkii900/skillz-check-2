24e48d6d1e5353cf2e35815b140ad18fce14df56
Mahkii900 <col.graff.battleschool@gmail.com>
2019-08-02 12:03:33 -0600

    added input functionality to wizard component

--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -2,12 +2,28 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
 export default class Wziard extends Component{
+    state = {
+        name: '',
+        address: '',
+        city: '',
+        state: '',
+        zipcode: null
+    }
     render() {
         return (
             <div>
-                <Link to={'/'}>
-                    <button>Cancel</button>
-                </Link>
+                <div>
+                    <input value={this.state.name} placeholder={'Property Name'} onChange={e => this.setState({name: e.target.value})}/>
+                    <input value={this.state.address} placeholder={'Address'} onChange={e => this.setState({address: e.target.value})}/>
+                    <input value={this.state.city} placeholder={'City'} onChange={e => this.setState({city: e.target.value})}/>
+                    <input value={this.state.state} placeholder={'State'} onChange={e => this.setState({state: e.target.value})}/>
+                    <input value={this.state.zipcode} placeholder={'Zipcode'} onChange={e => this.setState({zipcode: e.target.value})}/>
+                </div>
+                <div>
+                    <Link to={'/'}>
+                        <button>Cancel</button>
+                    </Link>
+                </div>
             </div>
         )
     }
